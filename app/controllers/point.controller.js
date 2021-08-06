f5464316d207b9dfcba98dbcc845daee39f36a93
emMercado <mercadobemma55@gmail.com>
2021-08-06 18:10:25 -0300

    add method: post,get,getById,deleteById,patch

--- app/controllers/point.controller.js
+++ app/controllers/point.controller.js
@@ -1,12 +1,47 @@
 const points = require('../../data.json');
-const pointSchema = require('../models/point.model')
+const pointSchema = require('../models/point.model');
 
 getPoint = (req, res) => {
   res.status(200).json(points.vaccination);
 }
 
-async function getPointById(req, res){
-  const id = req.params.pointSchemaId;
+async function getPlaces(req, res) {
+  pointSchema.find()
+    .exec()
+    .then(docs => {
+      console.log(docs);
+      if (docs.length >= 0) {
+        res.status(200).json(docs);
+      } else {
+        res.status(404).json({
+          message: "No entries found"
+        })
+      }
+    })
+    .catch(err => {
+      console.log(err.message);
+      res.status(500).json({
+        error: err
+      })
+    });
+}
+
+async function getPointById(req, res, next) {
+  const id = req.params.id;
+  pointSchema.findById(id)
+    .exec()
+    .then(doc => {
+      console.log("From database", doc);
+      if (doc) {
+        res.status(200).json({ doc });
+      } else {
+        res.status(404).json({ message: 'No valid entry found for provided ID' });
+      }
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({ error: err })
+    });
 }
 
 async function postPoint(req, res) {
@@ -29,5 +64,48 @@ async function postPoint(req, res) {
   )
 }
 
+async function deletePlace(req, res, next) {
+  const id = req.params.id;
+  pointSchema.deleteOne({ _id: id })
+    .exec()
+    .then(result => {
+      res.status(200).json(result);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
+};
 
-module.exports = { getPoint, getPointById ,postPoint };
+async function patchPlace(req, res) {
+  const id = req.params.id;
+  const updateOps = {};
+  for (const ops of req.body) {
+    updateOps[ops.name] = 
+      ops.value
+  }
+  pointSchema.updateMany({ _id: id }, {
+    $set: updateOps /* {
+      name: req.body.newName,
+      address: req.body.newAddress,
+      latitude: req.body.newLatitude,
+      longitude: req.body.newLongitude,
+      url: req.body.newUrl,
+    } */
+  })
+    .then(result => {
+      console.log(result);
+      res.status(200).json(result);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    })
+}
+
+
+module.exports = { getPoint, getPlaces, getPointById, postPoint, deletePlace, patchPlace };
--- app/routes/points.routes.js
+++ app/routes/points.routes.js
@@ -1,7 +1,6 @@
 const controller = require("../controllers/point.controller");
 
 module.exports = function(app){
-  
     app.get(
         "/point/",
         controller.getPoint
@@ -10,9 +9,20 @@ module.exports = function(app){
         "/point/:id",
         controller.getPointById
     );
+    app.get(
+        "/places/",
+        controller.getPlaces
+    );
     app.post(
         "/add_point/",
         controller.postPoint
     );
-
+    app.delete(
+        "/deletePlace/:id",
+        controller.deletePlace
+    )
+    app.patch(
+        "/patchPlace/:id",
+        controller.patchPlace
+    )
 };
