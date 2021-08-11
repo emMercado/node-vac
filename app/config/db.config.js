2b8b8a14999ef0018ded6e778dcdb81805804618
emMercado <mercadobemma55@gmail.com>
2021-08-11 15:38:36 -0300

    fix db config

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -2,7 +2,12 @@ const dbUser = process.env.DB_USER;
 const dbPass = process.env.DB_PASSWORD;
 const dbName = 'FinalProyect';
 const dbUri = `mongodb+srv://${dbUser}:${dbPass}@cluster0.55l2k.mongodb.net/${dbName}?retryWrites=true&w=majority`;
-const mongooseOptions = {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true};
+const mongooseOptions = {
+        useNewUrlParser: true,
+        useUnifiedTopology: true, 
+        useCreateIndex: true,
+        useFindAndModify: false,
+    };
 
 
 module.exports = {
